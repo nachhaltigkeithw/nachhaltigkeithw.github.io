9d77e430cbd63f959bf8a2de9ae03a7db0d9f02c
hw <nachhaltigkeithw@protonmail.com>
2020-09-12 17:31:32 +0200

    Updates

---
+++ precache-manifest.1c4a1c285e0cfce755b5f46d7ac51bf2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0e67c180fc8d2001e709f3930431a1b",
+    "url": "/nachhaltigkeithw.github.io/index.html"
+  },
+  {
+    "revision": "eba83a761ce454d406e2",
+    "url": "/nachhaltigkeithw.github.io/static/css/main.c6dc8d6a.chunk.css"
+  },
+  {
+    "revision": "83709495b281d989f1c4",
+    "url": "/nachhaltigkeithw.github.io/static/js/2.d2d794f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nachhaltigkeithw.github.io/static/js/2.d2d794f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eba83a761ce454d406e2",
+    "url": "/nachhaltigkeithw.github.io/static/js/main.1eff61ce.chunk.js"
+  },
+  {
+    "revision": "c7c9c172b08c3cd360f6",
+    "url": "/nachhaltigkeithw.github.io/static/js/runtime-main.1a7ebe20.js"
+  },
+  {
+    "revision": "b1edcbc77fe5cd881fa42182d375e7c2",
+    "url": "/nachhaltigkeithw.github.io/static/media/consumer.b1edcbc7.png"
+  },
+  {
+    "revision": "b8505dfc590f489e720e1aca29d7bd7c",
+    "url": "/nachhaltigkeithw.github.io/static/media/global.b8505dfc.png"
+  },
+  {
+    "revision": "f35991ec44f337f4e314b1524b90ccae",
+    "url": "/nachhaltigkeithw.github.io/static/media/health.f35991ec.png"
+  },
+  {
+    "revision": "efce2859224ec2115b614563fde2d75d",
+    "url": "/nachhaltigkeithw.github.io/static/media/hw-header.efce2859.png"
+  },
+  {
+    "revision": "ccda0abd490e08f4c880344ec8e80430",
+    "url": "/nachhaltigkeithw.github.io/static/media/idea.ccda0abd.png"
+  },
+  {
+    "revision": "cd78b2bba7772182c0cf464270c2c708",
+    "url": "/nachhaltigkeithw.github.io/static/media/infrastructure.cd78b2bb.png"
+  },
+  {
+    "revision": "1f977958325d439f37296fbbb40b85a5",
+    "url": "/nachhaltigkeithw.github.io/static/media/water-drop.1f977958.png"
+  }
+]);
